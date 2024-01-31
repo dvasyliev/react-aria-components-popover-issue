3d5030b68c9413c7b294f75f210f45fec99a57a1
Dmytro Vasyliev <dvasyliev.dev@gmail.com>
2024-01-31 13:02:47 +0100

    added story example

---
+++ src/SelectWithHint.stories.tsx
@@ -0,0 +1,8 @@
+import { SelectWithHint } from "./SelectWithHint";
+
+export default {
+  title: "SelectWithHint",
+  component: SelectWithHint,
+}
+
+export const Default = () => <SelectWithHint />;
